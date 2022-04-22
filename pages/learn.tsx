b352ffff9e4831df57ae018c0fd2460a8259a7be
trisdauvergne <tristan.dauvergne@gmail.com>
2022-04-22 10:59:17 +0200

    Initial commit after creating next project with React and TypeScript. Added data and image files to project from brief

---
+++ pages/learn.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Learn: React.FC = () => {
+  return <h1>Learn Page</h1>;
+};
+
+export default Learn;
---
+++ src/components/carTile/CarTile.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CarTile: React.FC = () => {
+  return <h1>Car Tile</h1>;
+};
+
+export default CarTile;
