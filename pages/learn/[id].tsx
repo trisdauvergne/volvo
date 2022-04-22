5f117493ca03b23a726e6f9a1522c0f82f151605
trisdauvergne <tristan.dauvergne@gmail.com>
2022-04-22 13:39:43 +0200

    Redeloping with error from car detail page removed

--- pages/learn/[id].tsx
+++ pages/learn/[id].tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { useRouter } from "next/router";
-import Link from 'next/link';
+// import Link from 'next/link';
 // import { ICarItem } from "../../src/interfaces/Car";
-import data from '../../public/api/cars.json';
+// import data from '../../public/api/cars.json';
 
 const Details: React.FC = () => {
   const router = useRouter();
   const { id } = router.query;
-  const carData = data.filter(car => car.id === id);
+  // const carData = data.filter(car => car.id === id);
 
-  if (carData) {
-    return (
-      <h1>Car: {carData[0].modelName}</h1>
-    )
-  }
+  // if (carData) {
+  //   return (
+  //     <h1>Car: {carData[0].modelName}</h1>
+  //   )
+  // }
 
   return <h1>ID {id}</h1>;
 };
